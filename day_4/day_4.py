b5c5f00360c225af61e38c765508150e5f980bec
jmplourde <2964972-jmplourde@users.noreply.gitlab.com>
2022-12-04 13:40:11 -0500

    Add part 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,5 +1,5 @@
-
 def main():
+    overlapping_pairs = 0
     inefficient_ranges_count = 0
     with open("input.txt", "r") as f:
         for line in f:
@@ -12,10 +12,13 @@ def main():
             first_part_range_len = int(first_part[-1])-int(first_part[0])+1
             second_part_range_len = int(second_part[-1])-int(second_part[0])+1
             overlap_len = len(overlap)
-            if overlap_len > 0 and (first_part_range_len == overlap_len or second_part_range_len == overlap_len):
-                inefficient_ranges_count += 1
+            if overlap_len > 0:
+                overlapping_pairs += 1
+                if first_part_range_len == overlap_len or second_part_range_len == overlap_len:
+                    inefficient_ranges_count += 1
 
     print(f"Part 1: there are {inefficient_ranges_count} inefficient ranges")
+    print(f"Part 2: there are {overlapping_pairs} overlapping pairs")
 
 
 def range_subset(range1, range2):
